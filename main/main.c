aa9a6ea6f65eed24c77a3d91e10f55cf0f7f6b65
RaulRangelMB <raulrmb@al.insper.edu.br>
2024-03-25 18:45:35 +0000

    Lab feito

--- main/main.c
+++ main/main.c
@@ -1,6 +1,3 @@
-/*
- * LED blink with FreeRTOS
- */
 #include <FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
@@ -13,6 +10,8 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define DEADZONE 30
+
 QueueHandle_t xQueueAdc;
 
 typedef struct adc {
@@ -20,21 +19,75 @@ typedef struct adc {
     int val;
 } adc_t;
 
+void x_task(void *p) {
+    adc_init();
+    adc_gpio_init(27);
+
+    int result;
+    while (1) {
+        adc_select_input(1);
+        result = (adc_read() - 2048)/8;
+        if (abs(result) > DEADZONE){
+           adc_t data = {0, result};
+           xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        }
+        else {
+            adc_t data = {0, 0};
+            xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
+
+void y_task(void *p) {
+    adc_init();
+    adc_gpio_init(26);
+
+    int result;
+    while (1) {
+        adc_select_input(0);
+        result = (adc_read() - 2048)/8;
+        if (abs(result) > DEADZONE){
+           adc_t data = {1, result};
+           xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        }
+        else{
+            adc_t data = {1, 0};
+            xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
 
 void uart_task(void *p) {
-    adc_t data;
+    adc_t data; 
 
-    while (1) {
-        xQueueReceive(xQueueAdc, &data, portMAX_DELAY);
+    while (1) {       
+        if(xQueueReceive(xQueueAdc, &data, portMAX_DELAY)){
+            int val = data.val;
+            int msb = val >> 8;
+            int lsb = val & 0xFF ;
+
+            uart_putc_raw(uart0, data.axis);
+            uart_putc_raw(uart0, lsb);
+            uart_putc_raw(uart0, msb);
+            uart_putc_raw(uart0, -1);
+        }
     }
 }
 
 int main() {
     stdio_init_all();
+    printf("Começando!!!\n");
+    adc_init();
 
     xQueueAdc = xQueueCreate(32, sizeof(adc_t));
 
-    xTaskCreate(uart_task, "uart_task", 4096, NULL, 1, NULL);
+    xTaskCreate(x_task, "x_task", 4095, NULL, 1, NULL);
+    xTaskCreate(y_task, "y_task", 4095, NULL, 1, NULL);
+    xTaskCreate(uart_task, "uart_task", 4095, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
